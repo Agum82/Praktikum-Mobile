1a59794c8d5281f4ddb21d27979cb09456849f03
Agum82 <2306010@itg.ac.id>
2026-03-11 17:50:21 +0700

    Pertemuan 3
    Updated UI components and fixed layout issues in MainPage. Changed app bar color, adjusted margins, and improved button styles.

--- Tugas3.dart
+++ Tugas3.dart
@@ -5,16 +5,23 @@ void main() {
 }
 
 class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(backgroundColor: Colors.amber, title: Text("FLEXBOX")),
+        appBar: AppBar(
+          backgroundColor: Colors.green, 
+          title: Text("FlexBox Flutter"),
+        ),
         body: SingleChildScrollView(
           child: Container(
-            margin: EdgeInsets.only(top: 20),
+            margin: EdgeInsets.only(top: 20, left: 10, right: 10),
             child: Column(
               children: [
+                
                 Flex(
                   direction: Axis.horizontal,
                   children: [
@@ -23,118 +30,180 @@ class MainPage extends StatelessWidget {
                       child: CircleAvatar(
                         radius: 20,
                         backgroundImage: NetworkImage(
-                          'https://picsum.photos/200/300',
+                          'https://picsum.photos/200',
                         ),
                       ),
                     ),
                     Expanded(
                       flex: 10,
                       child: Text(
-                        'selamat datang Di Toko',
-                        style: TextStyle(fontSize: 16, fontWeight: .bold),
+                        'Selamat Datang!',
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                       ),
                     ),
                   ],
                 ),
                 SizedBox(height: 20),
+
+                
                 Stack(
                   children: [
                     Image.network(
                       'https://picsum.photos/400/200',
                       fit: BoxFit.cover,
-                      width: .infinity,
+                      width: double.infinity,
+                      height: 200, 
                       colorBlendMode: BlendMode.darken,
                     ),
                     Positioned(
                       bottom: 16,
                       left: 16,
                       child: Text(
-                        'Flash',
+                        'FLASH SALE UP TO 50%',
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 20,
-                          fontWeight: .bold,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: .spaceEvenly,
-                  children: [
-                    TextButton(
-                      onPressed: () {},
-                      style: ButtonStyle(backgroundColor: .all(Colors.green)),
-                      child: Text(
-                        "baju",
-                        style: TextStyle(color: Colors.white),
+                SizedBox(height: 20),
+
+                
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(Colors.green),
+                          shape: WidgetStateProperty.all(StadiumBorder()),
+                        ),
+                        child: Text("Baju", style: TextStyle(color: Colors.white)),
                       ),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      style: ButtonStyle(backgroundColor: .all(Colors.green)),
-                      child: Text(
-                        "bahu",
-                        style: TextStyle(color: Colors.white),
+                      SizedBox(width: 8),
+                      TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(Colors.green),
+                          shape: WidgetStateProperty.all(StadiumBorder()),
+                        ),
+                        child: Text("Celana", style: TextStyle(color: Colors.white)),
                       ),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      style: ButtonStyle(backgroundColor: .all(Colors.green)),
-                      child: Text(
-                        "balo",
-                        style: TextStyle(color: Colors.white),
+                      SizedBox(width: 8),
+                      TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(Colors.green),
+                          shape: WidgetStateProperty.all(StadiumBorder()),
+                        ),
+                        child: Text("Sepatu", style: TextStyle(color: Colors.white)),
                       ),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      style: ButtonStyle(backgroundColor: .all(Colors.green)),
-                      child: Text(
-                        "bola",
-                        style: TextStyle(color: Colors.white),
+                      SizedBox(width: 8),
+                      TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(Colors.green),
+                          shape: WidgetStateProperty.all(StadiumBorder()),
+                        ),
+                        child: Text("Aksesoris", style: TextStyle(color: Colors.white)),
                       ),
-                    ),
-                  ],
+                      SizedBox(width: 8),
+                      TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(Colors.green),
+                          shape: WidgetStateProperty.all(StadiumBorder()),
+                        ),
+                        child: Text("Elektronik", style: TextStyle(color: Colors.white)),
+                      ),
+                      SizedBox(width: 8),
+                      TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(Colors.green),
+                          shape: WidgetStateProperty.all(StadiumBorder()),
+                        ),
+                        child: Text("Buku", style: TextStyle(color: Colors.white)),
+                      ),
+                    ],
+                  ),
                 ),
                 SizedBox(height: 20),
+
+                
                 GridView.count(
                   crossAxisCount: 2,
                   mainAxisSpacing: 10,
                   crossAxisSpacing: 10,
                   shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  childAspectRatio: 0.65, 
                   children: [
-                    Image.network(
-                      'https://picsum.photos/100/200',
-                      fit: BoxFit.cover,
-                      width: .infinity,
-                      height: 200,
+                    Card(
+                      child: Column(
+                        children: [
+                          Image.network(
+                            'https://picsum.photos/id/21/200/200', 
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: 200, 
+                          ),
+                          SizedBox(height: 8),
+                          Text("Sepatu"),
+                          Text("Rp. 200.000"),
+                        ],
+                      ),
                     ),
-                    Text("sweater"),
-                    Text("Rp. 200.000"),
-                    Image.network(
-                      'https://picsum.photos/id/237/200/300',
-                      fit: BoxFit.cover,
-                      width: .infinity,
-                      height: 200,
+                    Card(
+                      child: Column(
+                        children: [
+                          Image.network(
+                            'https://picsum.photos/id/1080/200/200', 
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: 200, 
+                          ),
+                          SizedBox(height: 8),
+                          Text("Satu Set Garpu"),
+                          Text("Rp. 20.000"),
+                        ],
+                      ),
                     ),
-                    Text("Anjing"),
-                    Text("Rp. 200.000"),
-                    Image.network(
-                      'https://picsum.photos/seed/picsum/300',
-                      fit: BoxFit.cover,
-                      width: .infinity,
-                      height: 200,
+                    Card(
+                      child: Column(
+                        children: [
+                          Image.network(
+                            'https://picsum.photos/id/20/200/200', 
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: 200, 
+                          ),
+                          SizedBox(height: 8),
+                          Text("Tas Ransel"),
+                          Text("Rp. 150.000"),
+                        ],
+                      ),
                     ),
-                    Text("Anjing"),
-                    Text("Rp. 200.000"),
-                    Image.network(
-                      'https://picsum.photos/300?grayscale',
-                      fit: BoxFit.cover,
-                      width: .infinity,
-                      height: 200,
+                    Card(
+                      child: Column(
+                        children: [
+                          Image.network(
+                            'https://picsum.photos/id/30/200/200',
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: 200, 
+                          ),
+                          SizedBox(height: 8),
+                          Text("Topi"),
+                          Text("Rp. 50.000"),
+                        ],
+                      ),
                     ),
-                    Text("Singa Laut"),
-                    Text("Rp. 200.000"),
                   ],
                 ),
               ],
