b82bba5ae8868d1057c15e36ed5ecd90910911a3
Agum82 <2306010@itg.ac.id>
2026-04-15 16:09:00 +0700

    Update Tugas6.dart

--- Tugas6.dart
+++ Tugas6.dart
@@ -22,46 +22,65 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController usernameController = TextEditingController();
-void login(){
-  if (_formKey.currentState!.validate()){
-    String username = usernameController.text;
-    ScaffoldMessenger.of(context,).showSnackBar(SnackBar(content: Text("Login berhasil: $username")));
+  final TextEditingController passwordController = TextEditingController();
+  void login() {
+    if (_formKey.currentState!.validate()) {
+      String username = usernameController.text;
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Login berhasil: $username")));
+    }
   }
-}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Form Login"),),
-      body: Padding(padding: const EdgeInsets.all(16),
-      child: Form(key: _formKey,
-      child: Column(
-        children: [TextFormField(
-          controller: usernameController,
-          decoration: const InputDecoration(
-            labelText: "username",
-            border: OutlineInputBorder(),
+      appBar: AppBar(title: const Text("Form Login")),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: usernameController,
+                decoration: const InputDecoration(
+                  labelText: "username",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "username wajib diisi";
+                  }
+                  if (value.length < 4 || value.isEmpty) {
+                    return "tidak Boleh kurang dari 4";
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  labelText: "passsword",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "tidak Boleh kurang dari 4";
+                  }
+                  if (value.length < 4 || value.isEmpty) {
+                    return "tidak Boleh kurang dari 4";
+                  }
+
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(onPressed: login, child: const Text("simpan")),
+            ],
           ),
-          validator: (value){
-            if (value == null || value.isEmpty){
-              return "username Wajib Diisi";
-            }
-            return null;
-          },
-
         ),
-        SizedBox(height: 16),
-        ElevatedButton(
-          onPressed: login, 
-          child: const Text("simpan")
-        
-         ),
-        
-      ],
-
-      ),),
-      
       ),
     );
   }
-  
 }
