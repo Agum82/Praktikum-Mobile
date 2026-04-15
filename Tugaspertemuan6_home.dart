24dcb0341e3a1bd8ea18938020d3676cf163885a
Agum82 <2306010@itg.ac.id>
2026-04-15 17:43:02 +0700

    Create Tugaspertemuan6_home.dart

---
+++ Tugaspertemuan6_home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final String username;
+
+  const HomePage({super.key, required this.username});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Kembali ke halaman Login
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Text(
+                "Selamat datang, $username!\n\nAnda berhasil login ke halaman home!",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
